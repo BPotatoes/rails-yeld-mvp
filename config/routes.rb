12d53a88b7316d2a0ae2f661a4d0b3ee2ec3c70c
bastien dubray <bastien.dubray@gmail.com>
2024-02-21 14:26:00 +0100

    finish to add review & front

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
 
   Rails.application.routes.draw do
     resources :restaurants do
-      resources :reviews, only: [:new]
+      resources :reviews, only: [:new, :create]
     end
   end
+end
